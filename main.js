fb4386f0b34aa48d2f38ac97020a6c9beaaf966e
OkarRojas <orojasb@unal.edu.co>
2025-09-03 18:23:13 -0500

    update nav, inicio

--- main.js
+++ main.js
@@ -1,8 +1,43 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const hamburger = document.getElementById('hamburger-icon');
-  const navMenu = document.getElementById('nav-menu');
+window.addEventListener("scroll", () => {
+    const navbar = document.getElementById("main-navbar");
+    if (window.scrollY > 30) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
 
-  hamburger.addEventListener('click', function () {
-    navMenu.classList.toggle('active');
-  });
+    const navbarr = document.getElementById("nav-menu-a");
+    if (window.scrollY > 30) {
+        navbarr.classList.add("scrolled");
+    } else {
+        navbarr.classList.remove("scrolled");
+    }
 });
+
+let subMenu = document.querySelector(".sub-menu");
+
+window.addEventListener("click", (e) => {
+    if (e.target.closest(".toggle")) {
+        subMenu.style.position = "fixed";
+        subMenu.style.display = "flex";
+    } else {
+        subMenu.style.position = "absolute";
+        subMenu.style.display = "none";
+    }
+});
+
+const caja = document.getElementById('texto-scroll');
+const imagen = document.getElementById('img2');
+
+caja.addEventListener("scroll", () => {
+  console.log("caja.scrollTop:", caja.scrollTop); // Depura el scroll
+  if (caja.scrollTop > 50 && caja.scrollTop <= 200) {
+    imagen.src = 'src/perfil.jpg'; // Imagen de prueba
+  } else if (caja.scrollTop > 200) {
+    imagen.src = 'src/img3.jpeg';
+  } else {
+    imagen.src = 'src/img2.jpeg';
+  }
+});
+
+
